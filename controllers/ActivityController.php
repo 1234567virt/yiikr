b632109bfd1bf863b7c654ceb774cf725cbb7047
vittes <vitte@bk.ru>
2019-07-12 16:51:20 +0300

    First commit.

---
+++ controllers/ActivityController.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\controllers;
+use yii\web\Controller;
+class ActivityController extends Controller
+{
+    public function actionIndex(){
+        return "Контроллер активностей";
+    }
+    public function actionCreate()
+    {
+        return "Создание активности";
+    }
+
+}?>
